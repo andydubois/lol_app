8ee6b4ae392128608aa9eb7866fe05e230bfa0e9
Andy DuBois <andydubois89@gmail.com>
2020-06-07 16:29:49 -0500

    set up index.js

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -19,6 +19,7 @@ class Home extends Component {
         this.setState({
             summonerName: event.target.value
         });
+        console.log(this.state)
     };
     render() {
         return (
--- src/index.js
+++ src/index.js
@@ -2,6 +2,49 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from '../src/components/App/App';
+import {createStore, combineReducers, applyMiddleware} from "redux";
+import {Provider} from "react-redux"
+
+//Import Saga Middleware
+import createSagaMiddleware from "redux-saga";
+import {takeEvery, put} from "redux-saga/effects";
+import axios from "axios"
+import logger from "redux-logger";
+
+//Create Saga Middleware
+const sagaMiddleware = createSagaMiddleware();
+
+// this line creates an array of all of redux middleware you want to use
+// we don't want a whole ton of console logs in our production code
+// logger will only be added to your project if your in development mode
+const middlewareList = process.env.NODE_ENV === 'development' ?
+  [sagaMiddleware, logger] :
+  [sagaMiddleware];
+
+const store = createStore(
+  // tells the saga middleware to use the rootReducer
+  // rootSaga contains all of our other reducers
+  rootReducer,
+  // adds all middleware to our project including saga and logger
+  applyMiddleware(...middlewareList)
+);
+
+// tells the saga middleware to use the rootSaga
+// rootSaga contains all of our other sagas
+sagaMiddleware.run(rootSaga);
+
+function* rootSaga() {
+  yield takeEvery("FETCH_SUMMONER", fetchSummoner);
+}
+
+
+function* fetchSummoner(action) {
+  console.log("summoner search");
+  try{
+    let summonerSearch = action.payload;
+    let response = yield axios.get(``)
+  }
+}
 
 
 ReactDOM.render(
