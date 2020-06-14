fd1031f4686229dcc38816f499d90f27e0188fd1
Andy DuBois <andydubois89@gmail.com>
2020-06-14 00:09:23 -0500

    mundo summoner get works

--- server/routes/summoner.router.js
+++ server/routes/summoner.router.js
@@ -8,26 +8,35 @@ let LeagueAPI = require("leagueapiwrapper");
 let leagueAPIKey = process.env.REACT_APP_RIOT_API_KEY;
 LeagueAPI = new LeagueAPI(leagueAPIKey, Region.NA);
 
-
-
-
-router.get('/:searchTerm', (req, res) => {
-  // return all categories
-    let searchTerm = req.params.searchTerm
-    console.log("THIS THE SUMMONER SEARCH TERM SERVER SIDE", searchTerm);
-  axios
-    .get(
-      `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${req.params.searchTerm}?api_key=${process.env.REACT_APP_RIOT_API_KEY}`
-    )
-    .then((response) => {
-      console.log("success server side summoner GET", response.data);
-      res.send(response.data);
+router.get("/:searchTerm", (req, res) => {
+  let summonerName = req.params.searchTerm
+  LeagueAPI.getSummonerByName(summonerName)
+    .then(function (accountInfo) {
+      // do something with accountInfo
+      console.log("success server side summoner GET", accountInfo);
+      res.send(accountInfo);
+      console.log(accountInfo);
     })
     .catch((error) => {
       console.log("error in server side summoner GET", error);
     });
 });
 
-
+// router.get("/:searchTerm", (req, res) => {
+//   // return all categories
+//   let searchTerm = req.params.searchTerm;
+//   console.log("THIS THE SUMMONER SEARCH TERM SERVER SIDE", searchTerm);
+//   axios
+//     .get(
+//       `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${req.params.searchTerm}?api_key=${process.env.REACT_APP_RIOT_API_KEY}`
+//     )
+//     .then((response) => {
+//       console.log("success server side summoner GET", response.data);
+//       res.send(response.data);
+//     })
+//     .catch((error) => {
+//       console.log("error in server side summoner GET", error);
+//     });
+// });
 
 module.exports = router;
